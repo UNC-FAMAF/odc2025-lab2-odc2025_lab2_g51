ad1fbdd487ebcd337d65edee654df9d3c6da20f7
dylan99516 <alexis.machado.933@mi.unc.edu.ar>
2025-06-02 22:55:48 -0300

    agregadas funciones linea_vertical, linea_diagonal y linea_diagonal2

--- ejercicio1/funciones.s
+++ ejercicio1/funciones.s
@@ -57,7 +57,28 @@ loopA:
 ret
 
 
+linea_vertical:
+    stp x29, x30, [sp, -16]!
+    mov x29, sp
+    stp x27, x28, [sp, -16]!
 
+
+    mov x27, x1   // eje X
+    mov x28, x2   // eje y
+    mov x9, x5    // cantidad de pixeles de largo
+
+    loopB:
+    mov x1, x27        // x actual
+    mov x2, x28        // y actual
+    bl pixel
+
+    add x28, x28, 1   // x27 = x27 + 1
+    sub x9, x9, 1     // x9 = x9 - 1
+    cbnz x9, loopB    // si x9 != 0, loopB
+
+    ldp x27, x28, [sp], 16
+    ldp x29, x30, [sp], 16
+ret
 
 llenar_cuadrado:
     stp x29, x30, [sp, -16]!
@@ -88,7 +109,74 @@ loopC:
     ldp x29, x30, [sp], 16
 ret
 
+//linea diagonal que avanza hacia abajo a la derecha
+linea_diagonal:
+    stp x29, x30, [sp, -16]!  // reserva memoria
+    mov x29, sp
+    stp x27, x28, [sp, -16]!
+    stp x25, x26, [sp, -16]!
+    stp x23, x24, [sp, -16]!
 
+
+    mov x27, x1   // eje X
+    mov x28, x2   // eje y
+    mov x25, x5    // largo de la linea
+    mov x24, x6     // cuantas lineas dura
+
+
+loopD:
+    mov x1, x27        // x actual
+    mov x2, x28        // y actual
+    mov x5, x25    // cantidad de pixeles a avanzar
+    bl linea_horizontal
+
+    add x27, x27, 1    // eje x - 1
+    add x28, x28, 1   // eje y - 1
+    sub x24, x24, 1     // cantidad de lineas - 1
+
+    cbnz x24, loopD        // si (x24 != 0), loopD
+
+    ldp x23, x24, [sp], 16      //libera memoria
+    ldp x25, x26, [sp], 16
+    ldp x27, x28, [sp], 16
+    ldp x29, x30, [sp], 16
+ret
+
+
+// mismo que la funcion anterior pero avanza hacia arriba a la derecha
+linea_diagonal2:
+    stp x29, x30, [sp, -16]!
+    mov x29, sp
+    stp x27, x28, [sp, -16]!
+    stp x25, x26, [sp, -16]!
+    stp x23, x24, [sp, -16]!
+
+
+    mov x27, x1   // eje X
+    mov x28, x2   // eje y
+    mov x25, x5    // largo de la linea
+    mov x26, x6     // cuantas lineas dura el loop
+    mov x24, 0     //contador
+
+
+loopE:
+    mov x1, x27        // x actual
+    mov x2, x28        // y actual
+    mov x5, x25    // cantidad de pixeles a avanzar
+    bl linea_horizontal
+
+    sub x28, x28, 1     // eje x - 1
+    add x27, x27, 1     // eje y - 1
+    add x24, x24, 1     // contador + 1
+    cmp x26, x24        // compara x26 y x24
+    b.ge loopE         // si x26 es mayor que x24, loopE. Sino termina
+
+
+    ldp x23, x24, [sp], 16     // libera memoria
+    ldp x25, x26, [sp], 16
+    ldp x27, x28, [sp], 16
+    ldp x29, x30, [sp], 16
+ret
 
 
 triangulo_rectangulo:
